8c06ef696f7d0d97465a7d6f85e739e8f8603ae3
Ilya Shamin <tetrarch93@gmail.com>
2024-09-26 16:07:57 +0400

    new file:   src/components/ProductCard.vue new file:   src/views/StoreView.vue

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,14 @@
+<script setup>
+  const props = defineProps('itemId', 'itemImage', 'itemPrice', 'itemName')
+</script>
+
+<template>
+  <div class="productCard_container">
+
+  </div>
+</template>
+
+
+<style scoped>
+  
+</style>
---
+++ src/views/StoreView.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="store_container">
+    <h2>Store</h2>
+    <div class="store_filters">
+      a
+    </div>
+    <div class="store_items">
+      <div class="placeholder">
+        placeholder
+      </div>
+      <div class="placeholder">
+        placeholder
+      </div>
+      <div class="placeholder">
+        placeholder
+      </div>
+      <div class="placeholder">
+        placeholder
+      </div>
+      <div class="placeholder">
+        placeholder
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .store_container{
+    margin-top: 10px;
+    border: 1px solid var(--bg-white);
+    background: var(--bg-white);
+    border-top-left-radius: var(--default-radius);
+    border-top-right-radius: var(--default-radius);
+    text-align: center;
+    height: max-content;
+  }
+  h2{
+    color: black;
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
+  .store_filters{
+    width: 200px;
+    height: 100px;
+    background: gray;
+    width: 100%;
+  }
+  .store_items{
+    padding: 20px;
+    display: flex;  
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+  .placeholder{
+    height: 400px;
+    width: 300px;
+    border-radius: var(--default-radius);
+    background-color: gray;
+    text-align: center;
+    margin: 10px;
+  }
+</style>
