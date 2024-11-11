799ead219ad5305d789a81ca9552e4f2a0b85aa7
Ilya Shamin <tetrarch93@gmail.com>
2024-11-11 16:57:40 +0400

    new file:   src/components/NotFound.vue

---
+++ src/components/NotFound.vue
@@ -0,0 +1,26 @@
+<script setup>
+    
+</script>
+
+<template>
+  <div class="container">
+    <h2>Whoops, wrong way!</h2>
+    <p>This page doesn`t exist!  </p>
+    <button><RouterLink to="/">Return to home</RouterLink></button>
+  </div>
+</template>
+
+
+<style scoped>
+.container{
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 60px;
+}
+
+.container p{
+  color: white;
+  font-size: 24px;
+}
+</style>
